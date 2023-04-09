0fdec4ab8df9d043e1e41399f62960ffa9f76e8d
Your Name <you@example.com>
2023-04-09 12:17:05 +0700

    up

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function count() {
     var inputStr = document.getElementById("inputStr").value.toUpperCase();
     var counts = {};
+    var countx = 0;
     for (var i = 0; i < inputStr.length; i++) {
       var char = inputStr.charAt(i);
       if (char.match(/[a-zA-Z]/)) {
+        countx++;
         counts[char] = counts[char] ? counts[char] + 1 : 1;
       }
     }
-    var result = "<tr><th>Kí tự</th><th>Đếm</th></tr>";
+    var result = `
+        <h4>Tổng số chữ cái: ${countx}</h4>
+    <tr><th>Kí tự</th><th>Đếm</th></tr>
+    `;
     for (var char in counts) {
       result += `<tr>
                     <td>${char}</td>
